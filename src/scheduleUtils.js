6ddab98115c9990d8f21c27d24abfd693390d395
Anand-Balaji282 <78731316+Anand-Balaji282@users.noreply.github.com>
2025-06-16 18:04:11 +0530

    Update scheduleUtils.js

--- src/scheduleUtils.js
+++ src/scheduleUtils.js
@@ -8,6 +8,18 @@ export const DAYS = [
   "Sunday"
 ];
 
+export const TIMES = [
+  "6:00 AM - 7:00 AM",
+  "7:00 AM - 8:00 AM",
+  "8:00 AM - 9:00 AM",
+  "9:00 AM - 10:00 AM",
+  "10:00 AM - 11:00 AM",
+  "11:00 AM - 12:00 PM",
+  "4:00 PM - 5:00 PM",
+  "5:00 PM - 6:00 PM",
+  "6:00 PM - 7:00 PM",
+  "7:00 PM - 8:00 PM"
+];
 export function getCurrentSchedule() {
   const s = localStorage.getItem("schedule");
   return s ? JSON.parse(s) : {};
